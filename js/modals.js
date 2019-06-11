6517bf424a2d58064292ac36132d26590dff5172
Galina <shaman282861@gmail.com>
2019-06-12 00:16:31 +0200

    bayan

--- js/modals.js
+++ js/modals.js
@@ -1,18 +1,28 @@
 const reviews = document.querySelector('.reviews__list'),
       overlay = document.querySelector('.overlay'),
       popupText = document.querySelector('.popup__text');
+      popupTitle = document.querySelector ('.popup__title')
+      modalClose = document.querySelector('.hamburger-menu-close__bars');
 
 reviews.addEventListener('click', e => {
   let element = e.target;
   
   if (element.tagName === "BUTTON") {
     let modalText = element.previousElementSibling.innerHTML;
-    
+    let modalTitle = element.previousElementSibling +10 .innerHTML;
+  
+    popupTitle.innerHTML = modalTitle;
     popupText.innerHTML = modalText;
     overlay.style.display = "block";
+   
   }
+
+  
+  
+  
 });
 
+//закрытие по esc
 document.addEventListener('keyup', e => {
   let keyName = e.keyCode;
   
